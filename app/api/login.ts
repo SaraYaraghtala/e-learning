d6029888cf8e7af7fff9443989a501c67455d2c2
Sara Yaraghtala <sarahyaraghtala@gmail.com>
2023-09-17 20:36:16 +0200

    create login.ts

---
+++ app/api/login.ts
@@ -0,0 +1,22 @@
+
+
+export default async(req:any, res:any)=>{
+    if (req.method==='POST'){
+        const {username , password}= req.body;
+
+        const response= await fetch ('http://localhost:1337/api/auth/local',{
+            headers :{
+                'Content-type':'application.json',
+            },
+            body: JSON.stringify({username,password}),
+        });
+        const data= await response.json();
+        if (response.ok){
+            res.status(200).json({token:data.jwt});
+        }else{
+            res.status(data.statusCode).json({error:data.message[0].message[0].message});
+        }
+    } else {
+        res.status(405).json({error:'Method not allow'})
+    }
+};
